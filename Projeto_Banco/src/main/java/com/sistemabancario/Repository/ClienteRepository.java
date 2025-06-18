00aa463fe70d8dfce20217cb2b08a1f307262f97
kdunog <carloseduardo.ndj7@gmail.com>
2025-06-18 13:02:01 -0300

    alterando para busca somente cpf

--- Projeto_Banco/src/main/java/com/sistemabancario/Repository/ClienteRepository.java
+++ Projeto_Banco/src/main/java/com/sistemabancario/Repository/ClienteRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sistemabancario.Model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long>{
-    Optional<Cliente> findByCPF(String CPF);
+    Cliente findByCPF(String CPF);
+    
 
 }
