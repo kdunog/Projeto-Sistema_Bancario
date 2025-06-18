6be590de918d923d1a66f2f2c4f7e1f03170ba6d
kdunog <carloseduardo.ndj7@gmail.com>
2025-06-18 13:02:37 -0300

    adicionando metodos post

--- Projeto_Banco/src/main/java/com/sistemabancario/Controller/LoginController.java
+++ Projeto_Banco/src/main/java/com/sistemabancario/Controller/LoginController.java
@@ -1,13 +1,28 @@
 package com.sistemabancario.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sistemabancario.Model.Cliente;
+import com.sistemabancario.Repository.ClienteRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping
 public class LoginController {
+
+    @Autowired
+    private ClienteRepository cRepository;
+
+    @GetMapping("/logincliente")
+    public String login(){
+        return "login";
+    }
 
     @GetMapping("/homesite")
     public String iniciosite(){
@@ -18,4 +33,33 @@ public class LoginController {
         return "cadastro";
     }
 
+    @PostMapping("/cadastrocliente")
+    public String cadastrarCliente(Cliente cliente, Model model){
+        if(cRepository.findByCPF(cliente.getCPF()) != null){
+            model.addAttribute("error", "CPF ja cadastrado");
+        }
+        else{
+
+            cRepository.save(cliente);
+            model.addAttribute("sucesso", "Você foi cadastrado com sucesso!");
+        }
+        // ESTA CADASTRANDO MAS N INDO PRO LOGIN
+        // NA VERDADE AQUI PRECISA RETORNAR PARA O LOGIN, CASO FOI CADASTRADO VAI CAIR NA PAGINA DE LOGIN, PRECISA SER REDIRECIONADO PARA LA
+        return "cadastro";
+    }
+
+    // APOS O CLIQUE NO LOGIN DEVE DIRECIONAR PRO DASHBOARD OQ N TA ACONTECENDO NECESSARIO CORREÇÃO
+    @PostMapping("/login")
+    public String logarCliente(Cliente cliente, Model model, HttpSession session){
+        Cliente c = cRepository.findByCPF(cliente.getCPF());
+        if(c != null && cliente.getSenha().equals(c.getSenha())){
+            session.setAttribute("Cliente logado", c);
+            return "redirect:/dashboard";
+        }
+        else{
+            model.addAttribute("erro", "Login invalido");
+            return "login";
+        }
+        
+    }
 }
