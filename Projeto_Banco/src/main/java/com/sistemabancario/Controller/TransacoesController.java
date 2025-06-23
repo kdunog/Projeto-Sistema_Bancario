0c663a3a3dd2d45311123d23ff0746fb07044946
kdunog <carloseduardo.ndj7@gmail.com>
2025-06-23 13:02:17 -0300

    add class transacoes controller/model

--- Projeto_Banco/src/main/java/com/sistemabancario/Controller/TransacoesController.java
+++ Projeto_Banco/src/main/java/com/sistemabancario/Controller/TransacoesController.java
@@ -3,6 +3,6 @@ package com.sistemabancario.Controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ClienteController {
+public class TransacoesController {
 
 }
---
+++ Projeto_Banco/src/main/java/com/sistemabancario/Model/Transacoes.java
@@ -0,0 +1,5 @@
+package com.sistemabancario.Model;
+
+public class Transacoes {
+
+}
