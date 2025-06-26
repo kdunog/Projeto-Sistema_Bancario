bbf4d8e65e5ced0c119b8952b39c70e6a4092188
kdunog <carloseduardo.ndj7@gmail.com>
2025-06-26 19:18:26 -0300

    add implementacao transaco e pag transferencia

--- Projeto_Banco/src/main/java/com/sistemabancario/Controller/TransacoesController.java
+++ Projeto_Banco/src/main/java/com/sistemabancario/Controller/TransacoesController.java
@@ -1,8 +1,34 @@
 package com.sistemabancario.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sistemabancario.Model.Cliente;
+import com.sistemabancario.Repository.ClienteRepository;
+
+
+//NECESSARIO IMPLEMENTACAO
 @Controller
 public class TransacoesController {
 
-}
+ @Autowired
+ public ClienteRepository cRepository;
+
+
+ @GetMapping("transferencia")
+ public String deposito(){
+    
+    
+    return "transferencia";
+ }
+    @PostMapping("/transferencia") 
+    public String transferencia(Cliente cliente, Model model){
+        
+        return null;
+        
+    }
+    
+ }
